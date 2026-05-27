1ab3347a7afed4dc5b750972a8eab5d0de97979d
dk99kodehode <dk99.kodehode@gmail.com>
2026-05-27 09:50:17 +0200

    Removed header and p

--- javascript.js
+++ javascript.js
@@ -1,7 +1,5 @@
 /*--------KALKULATOR------*/
 const display = document.getElementById("display");
-const paragraph = document.createElement("p");
-const heading = document.createElement("h2");
 
 function appendToDisplay(input) {
   display.value += input;
@@ -14,13 +12,3 @@ function clearDisplay() {
 function calculate() {
   display.value = eval(display.value);
 }
-
-heading.style.fontsize = "42px";
-heading.textContent = "Hey im Dom, how are you?";
-
-document.body.append(heading);
-
-paragraph.style.fontsize = "42px";
-paragraph.textContent = "Skadoosh youve been added";
-
-document.body.append(paragraph);
